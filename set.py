b329d54b0d9c82cf41d4ee0046e7e2d25fb1d7c8
alaminchowdhury1122 <alonealamin109@gmail.com>
2025-10-16 21:08:54 +0600

    set_union_intersection

--- set.py
+++ set.py
@@ -1,4 +1,10 @@
 S = {}    # This is empty set
 S = {1, 3, 5} #This is set (set is saparate by ,)
+T= {1,2,3,4,1,3,5,7,89,32,3215432,543,1,2,3,5}
 e = set() # This is empty set
+print(S,T)
+
+#add method
+S.add(111)
 print(S)
+print(type(S))
